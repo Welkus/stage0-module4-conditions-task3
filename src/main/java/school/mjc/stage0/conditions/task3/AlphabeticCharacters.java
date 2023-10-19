a3d09de4e6c6855ccdaa5b607956b011b936cf48
Welkus <vdelkus@gmail.com>
2023-10-19 16:16:58 +0300

    Test Done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        System.out.println(
+                Character.toString(character).matches("[AaEeIiOoUu]") ? "Vowel" :
+                !Character.toString(character).matches("[a-zA-Z]") ? "wrong alphabet!" : "Consonant"
+
+        );
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double afterTax;
+        if (salary < 0) System.out.println("wrong input!");
+        else {
+            afterTax =
+                    (salary <= 10000) ? (salary * 0.85) :
+                            (salary <= 20000) ?(salary * 0.82) :
+                                    (salary * 0.8);
+            System.out.println(afterTax);
+        }
+
     }
 }
